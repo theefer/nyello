49b9cb11cf36cb44619dfb5cd7e4265a8784faf2
Sebastien Cevey <seb@cine7.net>
2005-11-27 22:49:21 +0100

    Cosmetic and perfectionist refactoring of product makers.

--- src/productmaker.hh
+++ src/productmaker.hh
@@ -1,10 +1,10 @@
 #ifndef __PRODUCTMAKER_HH__
 #define __PRODUCTMAKER_HH__
 
-using namespace std;
-
 #include <cstring>
 #include <xmmsclient/xmmsclient.h>
+
+using namespace std;
 
 
 template <class T>
@@ -16,27 +16,11 @@ public:
 
 
 template <class T, int (*extractor)(xmmsc_result_t*, T*)>
-class ComparatorProduct : public ProductMaker<bool> {
+class PrimitiveProduct : public ProductMaker<T> {
 public:
-  ComparatorProduct(T value);
-
-  bool ComparatorProduct::create(xmmsc_result_t* res);
-
-private:
-  int value;
+  virtual T create(xmmsc_result_t* res);
 };
 
-
-
-class StringMatcherProduct : public ProductMaker<bool> {
-public:
-  StringMatcherProduct(char* str);
-
-  bool StringMatcherProduct::create(xmmsc_result_t* res);
-
-private:
-  char* str;
-};
 
 
 template <class T>
@@ -44,6 +28,7 @@ class ObjectProduct : public ProductMaker<T*> {
 public:
   virtual T* create(xmmsc_result_t* res);
 };
+
 
 
 template <class T, class X>
@@ -59,17 +44,34 @@ private:
 
 
 
+template <class T, int (*extractor)(xmmsc_result_t*, T*)>
+class ComparatorProduct : public ProductMaker<bool> {
+public:
+  ComparatorProduct(T value);
+
+  virtual bool create(xmmsc_result_t* res);
+
+private:
+  T value;
+};
+
+
+
+class StringMatcherProduct : public ProductMaker<bool> {
+public:
+  StringMatcherProduct(char* str);
+
+  virtual bool create(xmmsc_result_t* res);
+
+private:
+  char* str;
+};
+
 
 
 /*  == And here comes the implementation, in the header file due to
        boring GCC limitations...  == */
 
-
-template <class T, int (*extractor)(xmmsc_result_t*, T*)>
-class PrimitiveProduct : public ProductMaker<T> {
-public:
-  virtual T create(xmmsc_result_t* res);
-};
 
 template <class T, int (*extractor)(xmmsc_result_t*, T*)>
 T
@@ -78,22 +80,6 @@ PrimitiveProduct<T, extractor>::create(xmmsc_result_t* res) {
   (*extractor)(res, &product);
   xmmsc_result_unref(res);
   return product;
-}
-
-
-
-
-template <class T, int (*extractor)(xmmsc_result_t*, T*)>
-ComparatorProduct<T, extractor>::ComparatorProduct(T _value) : value(_value) {
-}
-
-template <class T, int (*extractor)(xmmsc_result_t*, T*)>
-bool
-ComparatorProduct<T, extractor>::create(xmmsc_result_t* res) {
-  T product;
-  (*extractor)(res, &product);
-  xmmsc_result_unref(res);
-  return (product == value);
 }
 
 
@@ -117,6 +103,24 @@ ComplexObjectProduct<T, X>::create(xmmsc_result_t* res) {
   T product(res, extraParam);
   return product;
 }
+
+
+
+template <class T, int (*extractor)(xmmsc_result_t*, T*)>
+ComparatorProduct<T, extractor>::ComparatorProduct(T _value) : value(_value) {
+}
+
+template <class T, int (*extractor)(xmmsc_result_t*, T*)>
+bool
+ComparatorProduct<T, extractor>::create(xmmsc_result_t* res) {
+  T product;
+  (*extractor)(res, &product);
+  xmmsc_result_unref(res);
+  return (product == value);
+}
+
+
+
 
 
 #endif  // __PRODUCTMAKER_HH__

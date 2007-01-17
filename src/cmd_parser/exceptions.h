344a859ba8d2383ccc0c73b3bf82fbf27e5d5a77
Sebastien Cevey <seb@cine7.net>
2007-01-17 02:03:35 +0100

    Renamed cmd_parser files, added exceptions code.

---
+++ src/cmd_parser/exceptions.h
@@ -0,0 +1,40 @@
+/* cmd_parser - a typed command parser
+ * Copyright (C) 2006  Sébastien Cevey
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+#ifndef CMD_PARSER_EXCEPTIONS_H
+#define CMD_PARSER_EXCEPTIONS_H
+
+#include <stdexcept>
+#include <string>
+
+
+namespace cmd_parser {
+
+	/** @class wrong_signature_error
+	 *  @brief Thrown if no signature associated to the command matches.
+	 */
+	class wrong_signature_error : public std::runtime_error
+	{
+		public:
+			explicit wrong_signature_error( const std::string& what_arg );
+
+	};
+
+}
+
+#endif  // CMD_PARSER_EXCEPTIONS_H
